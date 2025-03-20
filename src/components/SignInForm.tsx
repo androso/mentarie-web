fab8cfaac65ef5201e620a275102bf41be337762
Zack-y11 <dev.zacknmr28@gmail.com>
2025-03-19 21:40:16 -0600

    update Google login API URL to use environment variable

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -28,7 +28,7 @@ export default function SignInForm({ onSwitchForm }: SignInFormProps) {
 
   const handleGoogleLoginSuccess = (response: CredentialResponse) => {
     const token = response.credential // Google token
-    fetch('/api/auth/google', {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/google`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
